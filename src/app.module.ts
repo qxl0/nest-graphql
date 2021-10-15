017e0f5820ec25d6a4e3e05a4f75574f142009bc
qxl0 <qxl0@yahoo.com>
2021-10-14 20:15:08 -0400

    graphql sever works, db connection works

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {
+    console.log('DB is: ', this.connection.isConnected);
+  }
+}
